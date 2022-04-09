3635791d2728fc6380b121e3caf7173c83910335
Pascal Bürkle <exxxodus.artz@gmail.com>
2022-04-09 22:32:03 +0200

    update and improve colors

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -2,35 +2,25 @@ const { Theme } = require("vscode-theme-composer");
 
 module.exports = new Theme()
   .setName("Dark")
-  .setBaseColors("#52a7d9", "#202020")
+  .setBaseColors("#2eaadc", "#202020")
   .setColors({
     tokens: {
       string: "#bde052",
       comment: "#997f66",
-      variable: "#e1e1e1",
-      // string: "#afd194",
+      variable: "#ffffffde",
       tag: "#e08594",
-      attribute: "#8eb7c3",
+      attribute: "#a38ec3",
       entityClassType: "#ecbc55",
       functionMethodCall: "#49bde0",
       functionMethodDefinition: "#49bde0",
-      keywordStorage: "#8eb7c3",
-      keywordControlOther: "#8eb7c3",
+      keywordStorage: "#a38ec3",
+      keywordControlOther: "#a38ec3",
       property: "#dde8eb",
-      this: "#dde8eb",
+      this: "#ffffffae",
       constNumber: "#ec9459",
       propertyKey: "#b9b9b9",
       punctuation: "#a8a8a8",
-      keywordOperator: "#8eb7c3",
-      keywordOperatorLogicalComparison: "#a2d0df",
+      keywordOperator: "#a38ec3",
+      keywordOperatorLogicalComparison: "#a38ec3",
     },
   });
-// .setTransformOverrideColors(true)
-// .modifyColorGeneration({
-//   colorTransform: (color) => {
-//     return color.spin(45);
-//   },
-// });
-// .modifyColorGeneration({
-//   brightnessStart: null,
-// });
